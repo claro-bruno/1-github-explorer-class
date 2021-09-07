2bf8a4a4de0cb37a9a1979a1c1f97a548c025a3a
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-07 05:42:26 -0300

    Propriedades do React

---
+++ src/components/RepositoryItem/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class RepositoryItem extends Component {
+
+  render() {
+    const { repository } = this.props;
+    const { name, description, link } = repository;
+    return (
+      <li>
+            <strong>{ name ?? 'Default' }</strong>
+            <p>{ description }</p>
+            <a href={ link }>
+              Acess Repository
+            </a>
+          </li>
+    );
+  }
+};
--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,19 +1,21 @@
+import RepositoryItem from "../RepositoryItem";
+
 export default function repositoryList(){
 
-  const repositoryName = 'unform';
+  const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform',
+  };
 
   return (
     <section className="repository-list">
       <h1>RepositoryList</h1>
       <ul>
-        <li>
-          <strong>Uniform</strong>
-          <p>Forms in ReactJS</p>
-          <p>{ repositoryName }</p>
-          <a href=" ">
-            Acess Repository
-          </a>
-        </li>
+        <RepositoryItem repository={ repository } />
+        <RepositoryItem repository={ repository } />
+        <RepositoryItem repository={ repository } />
+        <RepositoryItem repository={ repository } />
       </ul>
     </section>
   );
